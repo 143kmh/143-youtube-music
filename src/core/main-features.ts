70bbec244aac30c2362cd748b3ddd2eb44a3138c
143kmh <236959400+143kmh@users.noreply.github.com>
2026-09-16 00:48:30 +0300

    refactor(core): use feature-native types in main runtime

--- src/core/main-features.ts
+++ src/core/main-features.ts
@@ -6,7 +6,7 @@ import { coreFeatures } from '@/core/features';
 import { LoggerPrefix, startFeature, stopFeature } from '@/utils';
 
 import type { BackendContext } from '@/types/contexts';
-import type { FeatureConfig, FeatureDef } from '@/types/plugins';
+import type { FeatureConfig, FeatureDef } from '@/types/features';
 
 const loadedFeatureMap: Record<
   string,
