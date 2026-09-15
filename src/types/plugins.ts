8fbedbf2d9eaff1841042811ba54c636f23c6c52
143kmh <surzhanyevhen@gmail.com>
2026-09-16 02:05:15 +0300

    Remove obsolete plugin type compatibility facade

--- src/types/plugins.ts
+++
@@ -1,5 +0,0 @@
-import type { FeatureConfig } from './features';
-
-// Final compatibility alias for the large settings module. Remove this file
-// once settings.ts moves to FeatureConfig directly.
-export type PluginConfig = FeatureConfig;
