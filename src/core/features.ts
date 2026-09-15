1538c5906397954bc14e6fc0b21ada446d6f334c
143kmh <236959400+143kmh@users.noreply.github.com>
2026-09-16 00:42:39 +0300

    refactor(core): use feature definitions in registry

--- src/core/features.ts
+++ src/core/features.ts
@@ -1,11 +1,11 @@
 import ui from '@/plugins/143-ui';
 import audio from '@/plugins/force-high-audio-quality';
 
-import type { PluginDef } from '@/types/plugins';
+import type { FeatureDef } from '@/types/plugins';
 
 export const coreFeatures: Record<
   string,
-  PluginDef<unknown, unknown, unknown>
+  FeatureDef<unknown, unknown, unknown>
 > = {
   '143-ui': ui,
   'force-high-audio-quality': audio,
