39a9650b7390f3b7359972b043541db173ccfd03
143kmh <surzhanyevhen@gmail.com>
2026-09-16 01:41:49 +0300

    refactor(lyrics): use feature config type

--- src/plugins/synced-lyrics/index.ts
+++ src/plugins/synced-lyrics/index.ts
@@ -6,13 +6,13 @@ import { menu } from './menu';
 import { renderer } from './renderer';
 import style from './style.css?inline';
 
-import type { SyncedLyricsPluginConfig } from './types';
+import type { SyncedLyricsFeatureConfig } from './types';
 
 export default createFeature<
   typeof backend,
   unknown,
   typeof renderer,
-  SyncedLyricsPluginConfig
+  SyncedLyricsFeatureConfig
 >({
   name: () => t('plugins.synced-lyrics.name'),
   description: () => t('plugins.synced-lyrics.description'),
