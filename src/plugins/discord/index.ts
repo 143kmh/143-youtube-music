d2cb26137d8227f0b31f1e8194b0f46745168ba4
143kmh <236959400+143kmh@users.noreply.github.com>
2026-09-15 22:11:36 +0300

    chore(discord): hide upstream github button by default

--- src/plugins/discord/index.ts
+++ src/plugins/discord/index.ts
@@ -52,7 +52,7 @@ export default createPlugin({
     'activityTimeoutEnabled': true,
     'activityTimeoutTime': 10 * 60 * 1000,
     'playOn\u0059\u006f\u0075\u0054\u0075\u0062\u0065\u004d\u0075\u0073\u0069\u0063': true,
-    'hideGitHubButton': false,
+    'hideGitHubButton': true,
     'hideDurationLeft': false,
     'statusDisplayType': StatusDisplayType.Details,
   } as DiscordPluginConfig,
