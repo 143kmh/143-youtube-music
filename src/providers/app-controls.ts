5ed33a43d33270812e6db895bbe202d26b944ca9
143kmh <236959400+143kmh@users.noreply.github.com>
2026-09-16 00:08:05 +0300

    refactor(143): remove inherited Pear app-control IPC

--- src/providers/app-controls.ts
+++ src/providers/app-controls.ts
@@ -1,5 +1,3 @@
-import path from 'node:path';
-
 import { app, BrowserWindow, ipcMain } from 'electron';
 
 import * as config from '@/config';
@@ -7,19 +5,14 @@ import * as config from '@/config';
 export const restart = () => restartInternal();
 
 export const setupAppControls = () => {
-  ipcMain.on('peard:restart', restart);
-  ipcMain.handle('peard:get-downloads-folder', () => app.getPath('downloads'));
-  ipcMain.on('peard:reload', () =>
+  ipcMain.removeAllListeners('app:reload');
+  ipcMain.on('app:reload', () =>
     BrowserWindow.getFocusedWindow()?.webContents.loadURL(config.get('url')),
-  );
-  ipcMain.handle('peard:get-path', (_, ...args: string[]) =>
-    path.join(...args),
   );
 };
 
 function restartInternal() {
   app.relaunch({ execPath: process.env.PORTABLE_EXECUTABLE_FILE });
-  // ExecPath will be undefined if not running portable app, resulting in default behavior
   app.quit();
 }
 
