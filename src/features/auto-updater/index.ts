226c055d7335cefc0df8695ba8204bea0543112d
143kmh <236959400+143kmh@users.noreply.github.com>
2026-09-16 21:40:53 +0300

    Wire updater status into 143 settings

--- src/features/auto-updater/index.ts
+++ src/features/auto-updater/index.ts
@@ -1,12 +1,17 @@
 import { createFeature } from '@/utils';
 
 import backend from './backend';
+import renderer from './renderer';
+import style from './style.css?inline';
 
 export default createFeature({
   name: () => 'Auto Updates',
-  description: () => 'Automatically download 143 Music updates from GitHub Releases.',
+  description: () =>
+    'Automatically download 143 Music updates from GitHub Releases.',
   config: {
     enabled: true,
   },
+  stylesheets: [style],
   backend,
+  renderer,
 });
